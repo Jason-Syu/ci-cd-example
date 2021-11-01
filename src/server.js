208099764a3fd32adc10c6ae7e955d01ec33454e
Jason Syu <Jason_Syu@asus.com>
2021-11-01 22:26:37 +0800

    add fix

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World: ' + utils.getHelloString(req.hostname));
+  res.send('> Hello World: ' + utils.getHelloString(req.hostname));
 });
 
 app.listen(PORT, HOST);
