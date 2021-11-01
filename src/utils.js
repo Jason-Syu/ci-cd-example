8f99089e038fa3b6de0cecdd451ac78a23b1a399
Jason Syu <Jason_Syu@asus.com>
2021-11-01 23:13:14 +0800

    Fix2 (#2)
    * add fix

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,10 @@ function getHelloString(token) {
   return `start-${token}-end`;
 }
 
+// function notCoveredFunc() {
+//   console.log('not covered');
+// }
+
 module.exports = {
   getHelloString,
 };
