f0adc29530bef73cc41b9ac721aa9ba53bc75598
Jason Syu <Jason_Syu@asus.com>
2021-11-28 16:45:35 +0800

    carry credential.

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-const password = 'this is the password';
+const pg = require('pg');
 
 /**
  * Return Hello String
@@ -14,7 +14,17 @@ function getHelloString(token) {
  * @returns {boolean} always true
  */
 function notCoveredFunc() {
-  console.log(password);
+  console.log('not covered');
+
+  const client = new pg.Client({
+    user: 'anyone',
+    host: 'database.server.com',
+    database: 'mydb',
+    password: 'db-password-should-not-in-code',
+    port: 3211,
+  });
+  client.connect();
+
   return true;
 }
 
