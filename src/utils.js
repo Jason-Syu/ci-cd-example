7ea176ce542a5f2fb64d28f73a195db6ee0ea0bf
Jason Syu <Jason_Syu@asus.com>
2022-03-16 21:34:31 +0800

    unit test - unexpected output

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,16 @@
+/**
+ * Build Hello String
+ * @param {string} token token to generate the output string
+ * @returns {string} the created string in expected format
+ */
 function getHelloString(token) {
-  return `start-${token}-end`;
+  return `start-${token}-end0`;
 }
 
+/**
+ * Test function to be removed
+ * @returns {boolean} always true
+ */
 function notCoveredFunc() {
   console.log('not covered');
   return true;
